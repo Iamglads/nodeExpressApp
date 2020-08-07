60a62c79f26aca3ff3ecd7a827e215cb311460ea
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-07 13:19:32 -0400

    auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+// ce middleware protégera les routes sélectionnées et vérifirera que l'utilisateur 
+// est authentifié avant d'autoriser l'envoi de ses requêtes 
+
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        if(req.body.userId && req.body.userId !== userId){
+            throw 'Invalid user ID';
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request')
+        });
+    }
+};
