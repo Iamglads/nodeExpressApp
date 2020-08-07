a962230cd021ba737445dc6d5a73ebd8edd33d90
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-06 23:05:12 -0400

    connect app ==> bdd

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
-
+mongoose.connect('mongodb+srv://glad:Gladetjuju-973@cluster0-ste1b.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 // une application est une série de fonction appelées middleware, chaque element de middleware recoit les objets request et response 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
-app.use( (req, res, next) => {
-    console.log('Requete reçue !')
-    next()
-})
+app.use(bodyParser.json());
 
-app.use( (req, res, next) => {
-    res.status(201)
-    next()
-})
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({ message: 'Objet crée !'});
+});
 
-app.use ( (req, res, next) => {
-    res.json({ message : 'Votre requete a bien été reçu !'});
-    next()
-})
-
-app.use( (req, res, next) => {
-    console.log('Réponses envoyée avec succès !')
-})
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
+});
 
 module.exports = app;
